4c1fdd9e76c4a604f8df27e59bfd54a8e3905c85
remontees <remontees@free.fr>
2012-12-08 16:35:59 +0100

    Add duckduckgo.js

---
+++ duckduckgo.js
@@ -0,0 +1,16 @@
+function unityReady() {
+    Unity.addAction("http://duckduckgo.com/", function () {
+       Unity.Notification.showNotification("Nouvelle recherche", "Vous vous apprêtez à lancer une nouvelle recherche.");
+    });
+    Unity.addAction("http://help.duckduckgo.com/customer/portal/articles/216441/", function() {
+        Unity.Notification.showNotification("Installation de l'extension", "Vous cherchez actuellement à installer DuckDuckGo sur votre navigateur web.");
+    });
+    Unity.Launcher.addAction("Internet", internetActionCallback);
+}
+var Unity = external.getUnityObject(1.0); 
+
+Unity.init({name: "DuckDuckGo",
+            iconUrl: "http://duckduckgo.com/assets/logo_homepage.normal.v102.png",
+            homepage: 'http://duckduckgo.com'
+            domain: 'duckduckgo.com'
+            onInit: unityReady});
